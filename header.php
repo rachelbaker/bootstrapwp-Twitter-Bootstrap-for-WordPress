5543d3c151ba29e9471e8c9153f86c984b7ee84b
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2012-01-15 10:11:45 +0700

    Move <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />    below wp_head() so style.css can overide the previous css declared.
    Signed-off-by: Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
    
 
-     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+     
 
 
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
@@ -53,7 +53,8 @@
     <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
     <?php wp_head(); ?>
-    
+	
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />   
 
   </head>
 
