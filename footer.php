fd6e61ec1f6711af22cd5cfcc3f8bc1b887c7a81
Rafael Funchal <rafaelfunchal@gmail.com>
2013-05-04 01:26:34 -0300

    Some Language support adjustments

--- footer.php
+++ footer.php
@@ -6,13 +6,13 @@
  * @subpackage Default_Theme
  * @since WP-Bootstrap 0.1
  *
- * Last Revised: July 16, 2012
+ * Last Revised: May 4, 2013
  */
 ?>
     <!-- End Template Content -->
       <footer>
 <div class="container">
-      <p class="pull-right"><a href="#">Back to top</a></p>
+      <p class="pull-right"><a href="#"><?php _e( 'Back to top', 'bootstrapwp' ); ?></a></p>
         <p>&copy; <?php bloginfo('name'); ?> <?php the_time('Y') ?></p>
           <?php
     if ( function_exists('dynamic_sidebar')) dynamic_sidebar("footer-content");
