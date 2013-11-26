80893ad197179814759a7864567235ee1ed0b6f1
Rachel Baker <rachel@rachelbaker.me>
2013-11-25 18:03:33 -0600

    Added breadcrumbs partial to single template.

--- single.php
+++ single.php
@@ -9,45 +9,45 @@
 get_header(); ?>
 <div class="container">
 
-	<?php if ( function_exists( 'bootstrapwp_breadcrumbs' ) ) : ?>
-		<?php bootstrapwp_breadcrumbs(); ?>
-	<?php endif; // end breadcrumbs ?>
+	<?php get_template_part( 'partials/breadcrumbs' ); ?>
 
 	<div class="row">
-    	<div class="col-md-8">
+		<div class="col-md-8">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<div <?php post_class(); ?>>
 					<div class="page-header">
-				        <h1><?php the_title();?></h1>
-				    </div>
+						<h1><?php the_title();?></h1>
+					</div>
 
-	            	<p class="meta"><?php echo bootstrapwp_posted_on();?></p>
+					<p class="meta"><?php echo bootstrapwp_posted_on();?></p>
 
-	            	<div class="entry-content">
-	            		<?php the_content(); ?>
-            		</div>
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
 
-	            	<?php edit_post_link(__('Edit', 'bootstrapwp'), '<button class="btn btn-default">', '</button>'); ?>
-            		<hr />
+					<?php edit_post_link(__('Edit', 'bootstrapwp'), '<button class="btn btn-default">', '</button>'); ?>
+					<hr />
 
-	            	<?php the_tags('<p>Tags: ', ', ', '</p>'); ?>
+					<?php the_tags('<p>Tags: ', ', ', '</p>'); ?>
 
-	            </div>
+					<?php if ( comments_open() || get_comments_number() ) :
+						// display comments template.
+						comments_template();
+					endif; ?>
 
-		        <?php comments_template(); ?>
-	        <?php endwhile; // end of the loop. ?>
+				</div>
 
+			<?php endwhile; // end of the loop. ?>
 
-
-		    <?php bootstrapwp_content_nav( 'nav-below' ); ?>
+			<?php bootstrapwp_content_nav( 'nav-below' ); ?>
 
 		</div>
 
-    	<?php get_sidebar('blog'); ?>
+		<?php get_sidebar('blog'); ?>
 
-    </div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
